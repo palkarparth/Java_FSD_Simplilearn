421018edf0f2d1f30f6664482554dbd4abed1eec
Parth Palkar <palkarparth24@gmail.com>
2022-03-03 11:03:41 +0530

    Adding Phase_1 Final Assessment

--- Phase1_Assessment/LockerCompany/src/main/java/com/simplilearn/lockerpvtltd/Main/LockerProject.java
+++ Phase1_Assessment/LockerCompany/src/main/java/com/simplilearn/lockerpvtltd/Main/LockerProject.java
@@ -3,7 +3,9 @@ package com.simplilearn.lockerpvtltd.Main;
 // Main Class
 public class LockerProject {
 	public static void main(String[] args) {
+		System.out.println("**********WELCOME TO LOCKER APPLICATION**********\n"
+				+ "----------Developer Name:- Parth Palkar----------");
 		WelcomeScreen welcomeScreen = new WelcomeScreen(); // creating object to display welcome screen
-		welcomeScreen.display(); 
+		welcomeScreen.display();
 	}
 }
--- Phase1_Assessment/LockerCompany/src/main/java/com/simplilearn/lockerpvtltd/Main/WelcomeScreen.java
+++ Phase1_Assessment/LockerCompany/src/main/java/com/simplilearn/lockerpvtltd/Main/WelcomeScreen.java
@@ -5,23 +5,22 @@ import java.util.Scanner;
 public class WelcomeScreen {
 	private void selectChoice() {
 		// Code for Welcome Screen
-		System.out.println("**********WELCOME TO LOCKER APPLICATION**********\n"
-				+ "----------Developer Name:- Parth Palkar----------");
+
 		Scanner scanner = new Scanner(System.in); // Scanner class
-		System.out.println("====================Main Menu====================\n"
-		        + "1.Retrive files in ascending order\n"
-				+ "2.Businees level operation on files:-\n" +
-				"  i.Add files\n" + "  ii.Delete files\n" + "  iii.Search files\n"
-				+ "  iv.Go to Main Menu\n" + 
-				"3.Close application.\n");
-		
+		System.out.println("====================Main Menu====================\n" + "1.Retrive files\n"
+				+ "2.Businees level operation on files:-\n" + "  i.Add files\n" + "  ii.Delete files\n"
+				+ "  iii.Search files\n" + "  iv.Go to Main Menu\n" + "3.Close application.\n");
+
 		System.out.print("Enter your choice->");
 		int choice = scanner.nextInt(); // Taking input from user
 
 		// Switch statement
 		switch (choice) {
 		case 1:
-
+			RetrieveFile retrieveFiles = new RetrieveFile();
+			retrieveFiles.display(); // displaying retrieve files class
+			System.out.println("\n");
+			display(); // displaying welcome screen
 			break;
 
 		case 2:
@@ -35,8 +34,8 @@ public class WelcomeScreen {
 			break;
 		}
 	}
-	
-	public void display() { 
+
+	public void display() {
 		selectChoice();
 	}
 }
